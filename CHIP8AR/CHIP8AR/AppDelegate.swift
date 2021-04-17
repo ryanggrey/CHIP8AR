fb4c86aa5e9b980aa0a1995a247491fbd9ce790a
Ryan Grey <ryan.g.grey@gmail.com>
2021-04-17 11:55:57 +0100

    Replace project with a SceneKit project

--- CHIP8AR/CHIP8AR/AppDelegate.swift
+++ CHIP8AR/CHIP8AR/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,15 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Use a UIHostingController as window root view controller.
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: contentView)
-        self.window = window
-        window.makeKeyAndVisible()
+        // Override point for customization after application launch.
         return true
     }
 
